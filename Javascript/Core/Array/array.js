d9a0ee54721373be14eef3c534229aef9fabee36
Ankush <ankushwadhwani222@gmail.com>
2025-10-29 11:37:05 +0530

    array changes

--- Javascript/Core/Array/array.js
+++ Javascript/Core/Array/array.js
@@ -1,5 +1,5 @@
 let arr = [1, "hello", true];
-arr.length; // 3
-arr.push(4); // adds element at end
-arr.pop(); // removes last element
-arr[1]; // "hello"
+arr.length; 
+arr.push(4); 
+arr.pop(); 
+arr[1]; 
