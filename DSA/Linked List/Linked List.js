d3c5d2b1c8be8ca55190abb7dd34537ecc93e7d8
Ankush <ankushwadhwani222@gmail.com>
2025-11-13 15:49:32 +0530

    linked list

---
+++ DSA/Linked List/Linked List.js
@@ -0,0 +1,59 @@
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }   
+    insert(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    delete(data) {
+        if (!this.head) {
+            return;
+        }
+        if (this.head.data === data) {
+            this.head = this.head.next;
+            return;
+        }
+        let current = this.head;
+        let previous = null;
+        while (current && current.data !== data) {
+            previous = current;
+            current = current.next;
+        }
+        if (current) {
+            previous.next = current.next;
+        }
+    }
+    display() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Example usage:
+const list = new LinkedList();
+list.insert(10);    
+list.insert(20);
+list.insert(30);
+list.display();
+list.delete(20);
+list.display();
